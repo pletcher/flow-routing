0542f19cd70937eec51193b493e495e3b71ba572
pletcher <charles.pletcher@gmail.com>
2017-07-12 12:00:27 -0400

    Fix constraints

--- package.js
+++ package.js
@@ -30,10 +30,10 @@ Package.onUse(function(api) {
   ], ['client', 'server']);
 
   api.use([
-     'react@0.14.1_1',
+     'react@15.0.0',
+     'react-mounter@1.2.0',
      'kadira:blaze-layout@2.3.0',
-     'kadira:react-layout@1.5.2',
-     'gwendall:blaze-to-react@0.1.2'
+     'gwendall:blaze-to-react@1.0.0'
   ], ['client', 'server'], { weak: true });
 
   api.addFiles([
